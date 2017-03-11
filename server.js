2a9f30e5300f983bf2479e988ce3f6f137b15532
Varsha Pathak <varsha.pathak@sjsu.edu>
2017-03-11 15:38:03 -0800

    email error

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/send', function (req, res) {
     );
 
     smtpTransport.sendMail({  //email options
-        from: "Gmail <spartaguidesjsu@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
+        from: "spartaguidesjsu@gmail.com", // sender address.  Must be the same as authenticated user if using Gmail.
         to: "varshapathak3100@gmail.com", // receiver
         subject: "Portfolio Message", // subject
         text: msg // body
